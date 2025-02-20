233bfcd931f9f29e19d600ca4491d238ae19306b
Jayastri Nivedha <113873647+jayanivi30@users.noreply.github.com>
2025-02-21 00:11:53 +0530

    Update hypotenuse.java

--- hypotenuse.java
+++ hypotenuse.java
@@ -10,7 +10,7 @@ public class hypotenuse {
 		System.out.println("Enter length of side b: ");
 		double b = sc.nextDouble();
 		double c = Math.sqrt(Math.pow(a, 2)+Math.pow(b, 2));
-		System.out.println("The hypotenuse of the triangle is : "+c);
+		System.out.println("The hypotenuse of the triangle is : "+c+"cm.");
 		sc.close();
 
 	}
