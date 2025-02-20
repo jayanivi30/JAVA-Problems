5e5b395ba875756178b93c09744fd7ee1819e816
Jayastri Nivedha <113873647+jayanivi30@users.noreply.github.com>
2025-02-21 00:09:43 +0530

    Add files via upload

---
+++ hypotenuse.java
@@ -0,0 +1,18 @@
+package Miniproj;
+import java.util.*;
+public class hypotenuse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter length of side a: ");
+		double a = sc.nextDouble();
+		System.out.println("Enter length of side b: ");
+		double b = sc.nextDouble();
+		double c = Math.sqrt(Math.pow(a, 2)+Math.pow(b, 2));
+		System.out.println("The hypotenuse of the triangle is : "+c);
+		sc.close();
+
+	}
+
+}
