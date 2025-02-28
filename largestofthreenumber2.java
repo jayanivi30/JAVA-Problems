729cb2404f8448a27b902b553c47cb943cfe22ac
Jayastri Nivedha <113873647+jayanivi30@users.noreply.github.com>
2025-02-28 23:19:21 +0530

    Add files via upload

---
+++ largestofthreenumber2.java
@@ -0,0 +1,17 @@
+package problems;
+import java.util.*;
+public class largestofthreenumber2 {
+	public static int main() {
+		int a=5;
+		int b=8;
+		int c=10;
+		int max;
+		max=(Math.max(a, Math.max(b, c)));
+		return max;
+		
+	}
+	public static void main(String args[]) {
+		System.out.print(main());
+		
+	}
+}
